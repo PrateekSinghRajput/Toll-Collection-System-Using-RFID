44ae0e55142b6a641efa499b2534d98ea0bd3aaa
Prateek Singh <92904643+PrateekSinghRajput@users.noreply.github.com>
2023-08-08 02:02:51 +0530

    Add files via upload

---
+++ Code/Code.ino
@@ -0,0 +1,69 @@
+//Prateek
+//wwww.prateeks.in
+//https://www.youtube.com/c/JustDoElectronics/videos
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+#define LED_G 5
+#define LED_R 4
+#define RELAY 3
+#define BUZZER 2
+#define ACCESS_DELAY 6000
+#define DENIED_DELAY 4000
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(RELAY, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  noTone(BUZZER);
+  digitalWrite(RELAY, LOW);
+  Serial.println("Put your card to the reader...");
+  Serial.println();
+}
+void loop() {
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  Serial.print("UID tag :");
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "A2 19 55 2D") {
+    Serial.println("Authorized access");
+    Serial.println();
+    delay(500);
+    digitalWrite(RELAY, HIGH);
+    digitalWrite(LED_G, HIGH);
+    delay(ACCESS_DELAY);
+    digitalWrite(RELAY, LOW);
+    digitalWrite(LED_G, LOW);
+  }
+
+  else {
+    Serial.println(" Access denied");
+    digitalWrite(LED_R, HIGH);
+    tone(BUZZER, 300);
+    delay(DENIED_DELAY);
+    digitalWrite(LED_R, LOW);
+    noTone(BUZZER);
+  }
+}
